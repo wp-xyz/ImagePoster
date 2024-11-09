647b66faec993a08db9fb8738fb538cdb39e251b
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-09 12:20:37 +0200

    fix makes files

--- make.ps1
+++ make.ps1
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env pwsh
 ##############################################################################################################
 
-Function PrivClipper {
+Function Priv-Clipper {
     Return "
 Usage: pwsh -File $($PSCommandPath) [OPTIONS]
 Options:
@@ -9,26 +9,68 @@ Options:
 "
 }
 
-Function PrivPrepare {
-    $VAR = @(
-        @{
-            Cmd = 'lazbuild'
-            Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
-            Path = "C:\Lazarus"
+Function Priv-Wget {
+    ForEach ($REPLY in $args) {
+        $params = @{
+            Uri = $REPLY
+            OutFile = (Split-Path -Path $REPLY -Leaf).Split('?')[0]
         }
-    )
-    ForEach ($REPLY in $VAR) {
-        If (-not (Get-Command $REPLY.Cmd -ea 'continue')) {
-            $params = @{
-                Uri = $REPLY.Url
-                OutFile = (Split-Path -Path $REPLY.Url -Leaf).Split('?')[0]
+        Invoke-WebRequest @params | Out-Null
+        Return $params.OutFile
+    }
+}
+
+Function Priv-Msiexec {
+    While ($Input.MoveNext()) {
+        $params = @{}
+        Switch ((Split-Path -Path $Input.Current -Leaf).Split('.')[-1]) {
+            'msi' {
+                $params = @{
+                    FilePath = 'msiexec'
+                    ArgumentList = '/passive', '/package', $Input.Current
+                }
             }
-            Invoke-WebRequest @params
-            Start-Process -PassThru -Wait -FilePath $params.OutFile -ArgumentList '/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES', '/NORESTART'
-            Remove-Item $params.OutFile
-            $env:PATH+=";$($REPLY.Path)"
-            Get-Command $REPLY.Cmd
+            'exe' {
+                $params = @{
+                    FilePath = $Input.Current
+                    ArgumentList = '/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES', '/NORESTART'
+                }
+            }
         }
+        Start-Process -PassThru -Wait @params
+        Remove-Item $Input.Current
+    }
+}
+
+Function Priv-LazBuild {
+    $VAR = @{
+        Cmd = 'lazbuild'
+        Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
+        Path = "C:\Lazarus"
+    }
+    If (-not (Get-Command $VAR.Cmd -ea 'continue')) {
+        Priv-Wget $VAR.Url | Priv-Msiexec
+        $env:PATH+=";$($VAR.Path)"
+        Get-Command $VAR.Cmd
+    }
+    If ( Test-Path -Path 'use\components.txt' ) {
+        Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--init'
+        Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--remote'
+        Get-Content -Path 'use\components.txt' | ForEach-Object {
+            If ((-not (Start-Process -ea 'continue' -Wait -FilePath 'lazbuild' -ArgumentList '--verbose-pkgsearch', $_)) -and
+                (-not (Start-Process -ea 'continue' -Wait -FilePath 'lazbuild' -ArgumentList '--add-package', $_)) -and
+                (-not (Test-Path -Path 'use\components.txt'))) {
+                    $OutFile = Priv-Wget "https://packages.lazarus-ide.org/$($_).zip"
+                    Expand-Archive -Path $OutFile -DestinationPath "use\$($_)" -Force
+                    Remove-Item $OutFile
+                }
+        }
+        Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
+            Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--add-package-link', $_.Name
+        }
+    }
+    Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'src' | ForEach-Object {
+        Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--no-write-project', '--recursive', '--build-mode=release', $_.Name
     }
 }
 
@@ -39,26 +81,16 @@ Function PrivMain {
     If ($args.count -gt 0) {
         Switch ($args[0]) {
             'build' {
-                PrivPrepare
-                If (Test-Path -Path 'use') {
-                    Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--init'
-                    Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--remote'
-                    Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
-                        Start-Process -PassThru -Wait -FilePath 'lazbuild' -ArgumentList '--add-package-link', $_.Name
-                    }
-                }
-                Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'src' | ForEach-Object {
-                    Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--no-write-project', '--recursive', '--build-mode=release', $_.Name
-                }
+                Priv-LazBuild
             }
             Default {
-                PrivClipper
+                Priv-Clipper
             }
         }
     } Else {
-        PrivClipper
+        Priv-Clipper
     }
 }
 
 ##############################################################################################################
-PrivMain @args
+Priv-Main @args
--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-##############################################################################################################
 
 function priv_clippit
 (
@@ -10,18 +9,38 @@ Options:
 EOF
 )
 
-function priv_dpkg
+function priv_lazbuild
 (
-    # Linux shell script to create .deb package
-    # Go to the repository local folder. For example, cd /home/User/ShellRemoteBot
-    declare -r STAGING_DIR=./deb.staging
-    find "${STAGING_DIR}" -type d -exec chmod 0755 {} +
-    find "${STAGING_DIR}" -type f -exec chmod 0644 {} +
-    find "${STAGING_DIR}/usr/bin" -type f -exec chmod 0755 {} +
-    echo "Installed-Size: $(du -s ${STAGING_DIR} | awk '{print $1;}')" >>"${STAGING_DIR}/DEBIAN/control"
-    gzip -9 -n 'deb.staging/usr/share/doc/tgshd/changelog.Debian'
-    read -rp "Input package filename: "
-    dpkg-deb --root-owner-group --build "${STAGING_DIR}" "${REPLY}.deb"
+    if ! (which lazbuild); then
+        source '/etc/os-release'
+        case ${ID:?} in
+            debian | ubuntu)
+                sudo apt-get update
+                sudo apt-get install -y lazarus
+                ;;
+        esac
+    fi
+    if [[ -f 'use/components.txt' ]]; then
+        git submodule update --init --recursive
+        git submodule update --recursive --remote
+        while read -r; do
+            if [[ -n "${REPLY}" ]] &&
+                ! (lazbuild --verbose-pkgsearch "${REPLY}") &&
+                ! (lazbuild --add-package "${REPLY}") &&
+                ! [[ -f "use/${REPLY}" ]]; then
+                    declare -A VAR=(
+                        [url]="https://packages.lazarus-ide.org/${REPLY}.zip"
+                        [out]=$(mktemp)
+                    )
+                    wget --output-document "${VAR[out]}" "${VAR[url]}" >/dev/null
+                    unzip -o "${VAR[out]}" -d "use/${REPLY}"
+                    rm --verbose "${VAR[out]}"
+                fi
+        done < 'use/components.txt'
+        find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
+    fi
+    find 'src' -type 'f' -name '*.lpi' \
+        -exec lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
 )
 
 function priv_main
@@ -29,30 +48,12 @@ function priv_main
     set -euo pipefail
     if ((${#})); then
         case ${1} in
-            build)
-                if ! (which 'lazbuild'); then
-                    source '/etc/os-release'
-                    case ${ID:?} in
-                        debian | ubuntu)
-                            sudo apt-get update
-                            sudo apt-get install -y lazarus
-                            ;;
-                    esac
-                fi
-                if [[ -d 'use' ]]; then
-                    git submodule update --recursive --init
-                    git submodule update --recursive --remote
-                    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
-                fi
-                find 'src' -type 'f' -name '*.lpi' -exec \
-                    lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
-                ;;
-            dpkg) priv_dpkg ;;
+            build) priv_lazbuild ;;
+            *) priv_clippit ;;
         esac
     else
         priv_clippit
     fi
 )
 
-##############################################################################################################
 priv_main "${@}" >/dev/null
